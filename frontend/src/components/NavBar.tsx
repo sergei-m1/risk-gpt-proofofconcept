da7bcc6f992286fd0135fe5d5db0f0f4d9dde2b1
Sergei <sergei.margolin1@gmail.com>
2023-06-06 20:32:52 +1000

    Updated the NavBar

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -1,12 +1,21 @@
-import { HStack, Image } from "@chakra-ui/react";
-import logo from "../assets/logo.webp";
+import { HStack, Text } from "@chakra-ui/react";
 import ColorModeSwitch from "./ColorModeSwitch";
+import { SiOpenai } from "react-icons/si";
+import { motion } from "framer-motion";
 
 const NavBar = () => {
   return (
-    <HStack justifyContent="space-between" padding="10px" bg="gray.400">
-      <Image src={logo} boxSize="60px" />
+    <HStack padding="15px" bg="gray.600">
       {/* <ColorModeSwitch /> */}
+      <motion.div
+        animate={{ rotate: 270 }} // Rotate the emblem 270 degrees
+        transition={{ repeat: Infinity, duration: 20 }} // Repeat the rotation animation indefinitely with a duration of 2 seconds
+      >
+        <SiOpenai size={45} color="white" />
+      </motion.div>
+      <Text color="white" fontSize="2xl" fontWeight="bold" paddingX={6}>
+        Risk GPT Prototype
+      </Text>
     </HStack>
   );
 };
