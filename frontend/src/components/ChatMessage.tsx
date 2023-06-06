d63bfa265efa49b8082f1244c225941cc6aa7344
Sergei <sergei.margolin1@gmail.com>
2023-06-06 20:12:02 +1000

    Updated formatting of ChatMessage component to include dynamic alignment and wrap

--- frontend/src/components/ChatMessage.tsx
+++ frontend/src/components/ChatMessage.tsx
@@ -3,20 +3,29 @@ import React from "react";
 import { Message } from "./ChatWindow";
 
 const ChatMessage = ({ type, text }: Message) => {
-  const bgColor: string = type === "sent" ? "green.200" : "blue.200";
-  const marginRightValue: number = type === "sent" ? 0 : 32;
-  const marginLeftValue: number = type === "sent" ? 32 : 0;
+  const isSentMessage = type === "sent";
+  const bgColor = isSentMessage ? "blue.100" : "blue.500";
+  const fontColor = isSentMessage ? "black" : "black";
+  const alignment = isSentMessage ? "flex-end" : "flex-start";
+
+  const messageStyle: React.CSSProperties = {
+    backgroundColor: bgColor,
+    color: fontColor,
+    borderRadius: 18,
+    margin: 2,
+    padding: "10px",
+    paddingRight: "20px",
+    paddingLeft: "20px",
+    maxWidth: "80%", // Limit the width to 80% of available space
+    alignSelf: alignment, // Align the message to the right or left
+    wordWrap: "break-word" as "break-word", // Break long words onto the next line
+  };
 
   return (
-    <Box
-      backgroundColor={bgColor}
-      marginRight={marginRightValue}
-      marginLeft={marginLeftValue}
-      borderRadius={15}
-      marginY={4}
-      padding={2}
-    >
-      <Text>{text}</Text>
+    <Box display="flex" justifyContent={alignment}>
+      <Box sx={messageStyle}>
+        <Text>{text}</Text>
+      </Box>
     </Box>
   );
 };
