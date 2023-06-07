5706b988c8df767d555d27ee0bf85833effdd7be
Sergei <sergei.margolin1@gmail.com>
2023-06-07 20:29:22 +1000

    Updated sidebar to align sides based on the section and description

--- frontend/src/components/SideBar.tsx
+++ frontend/src/components/SideBar.tsx
@@ -11,8 +11,8 @@ const SideBar = () => {
 
   return (
     <Box paddingLeft={3} paddingY={10}>
-      <VStack align="left" spacing={4}>
-        <HStack>
+      <HStack>
+        <VStack align="left" spacing={4}>
           <Text
             paddingY={2}
             fontWeight={activeSection === "about" ? "bold" : "normal"}
@@ -21,18 +21,6 @@ const SideBar = () => {
           >
             About
           </Text>
-          <Collapse in={activeSection === "about"}>
-            <Text paddingY={2}>
-              Risk GPT is a Large Language Model (LLM) powered chat bot. It is
-              using knowledge contained in a local repository of documents to
-              answer questions on topics of Finance and Risk. This is a proof of
-              concept model that has not seen too many documents and therefore
-              its capabilities are limited. To understand what topics Risk GPT
-              Prototype has knowldege on, please refer to the Knowledge section.
-            </Text>
-          </Collapse>
-        </HStack>
-        <HStack>
           <Text
             paddingY={2}
             fontWeight={activeSection === "technologies" ? "bold" : "normal"}
@@ -41,15 +29,7 @@ const SideBar = () => {
           >
             Technologies
           </Text>
-          <Collapse in={activeSection === "technologies"}>
-            <Text paddingY={2}>
-              This prototype is powered by the following technologies: 1.
-              LangChain 2. LLama Index 3. Vicuna Open Source LLM model 4. Flask
-              Restful API 5. Typescript 6. Chakra UI
-            </Text>
-          </Collapse>
-        </HStack>
-        <HStack>
+
           <Text
             paddingY={2}
             fontWeight={activeSection === "contact" ? "bold" : "normal"}
@@ -58,15 +38,34 @@ const SideBar = () => {
           >
             Contact
           </Text>
-          <Collapse in={activeSection === "contact"}>
-            <Text paddingY={2}>
-              For more information on this prototype please contact Sergei
-              Margolin at example@email.com. You can find the source code under
-              MIT license here: example@github.com
+        </VStack>
+        <Box>
+          <Collapse in={activeSection === "about"}>
+            <Text paddingY={2} paddingX={6} alignItems="flex-start">
+              Risk GPT is a Large Language Model (LLM) powered chat bot. It is
+              using knowledge contained in a local repository of documents to
+              answer questions on topics of Finance and Risk. This is a proof of
+              concept model that has not seen too many documents and therefore
+              its capabilities are limited. To understand what topics Risk GPT
+              Prototype has knowldege on, please refer to the Knowledge section.
             </Text>
           </Collapse>
-        </HStack>
-      </VStack>
+          <Collapse in={activeSection === "technologies"}>
+            <Text paddingY={2} paddingX={6}>
+              This prototype is powered by the following technologies: 1.
+              LangChain 2. LLama Index 3. Vicuna Open Source LLM model 4. Flask
+              Restful API 5. Typescript 6. Chakra UI
+            </Text>
+          </Collapse>
+          <Collapse in={activeSection === "contact"}>
+            <Text paddingY={2} paddingX={6}>
+              For more information on this prototype please contact Sergei
+              Margolin at example@email.com. You can find the source code under
+              MIT license here: example@github.com.
+            </Text>
+          </Collapse>
+        </Box>
+      </HStack>
     </Box>
   );
 };
