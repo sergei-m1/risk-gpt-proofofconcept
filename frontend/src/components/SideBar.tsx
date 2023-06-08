7e79ee62cf9beb42de52d910aaa04d808ed7e5af
Sergei <sergei.margolin1@gmail.com>
2023-06-08 20:40:34 +1000

    Decoupled SideBar input data into a separate object

--- frontend/src/components/SideBar.tsx
+++ frontend/src/components/SideBar.tsx
@@ -1,5 +1,5 @@
 import { Box, VStack, Text, Collapse, HStack } from "@chakra-ui/react";
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import SideBarSection from "./SideBarSection";
 import SideBarDescription from "./SideBarDescription";
 import { HiChevronDoubleLeft } from "react-icons/hi";
@@ -7,6 +7,13 @@ import { HiChevronDoubleLeft } from "react-icons/hi";
 interface Props {
   closed: boolean;
   onToggleSidebar: () => void;
+}
+
+interface SideBarData {
+  [key: string]: {
+    title: string;
+    text: string;
+  };
 }
 
 // color="gray.800"
@@ -20,41 +27,48 @@ const SideBar = ({ closed, onToggleSidebar }: Props) => {
     }
   };
 
+  const sideBarData: SideBarData = {
+    about: {
+      title: "About",
+      text: "Risk GPT is a Large Language Model (LLM) powered chat bot. It is using knowledge contained in a local repository of documents to answer questions on topics of Finance and Risk. This is a proof of concept model that has not seen too many documents and therefore its capabilities are",
+    },
+    technologies: {
+      title: "Technologies",
+      text: "This prototype is powered by the following technologies: 1. LangChain 2. LLama Index 3. Vicuna Open Source LLM model 4. Flask Restful API 5. Typescript 6. Chakra UI",
+    },
+    contact: {
+      title: "Contact",
+      text: "For more information on this prototype please contact Sergei Margolin at example@email.com. You can find the source code under MIT license here: example@github.com.",
+    },
+  };
+
+  const activeDescription = sideBarData[activeSection];
+
+  const filteredDescriptions = Object.entries(sideBarData).filter(
+    ([key]) => key === activeSection
+  );
+
+  const [activeDescriptionKey, activeDescriptionValue] =
+    filteredDescriptions[0];
+
   return (
     <Box paddingLeft={3} paddingY={10}>
       <HStack>
         <VStack align="left" spacing={4}>
-          <>
+          {Object.entries(sideBarData).map(([key, value]) => (
             <SideBarSection
-              active={activeSection === "about"}
-              title="About"
-              onClick={() => handleSectionClick("about")}
+              key={key}
+              active={activeSection === key}
+              title={value.title}
+              onClick={() => handleSectionClick(key)}
             />
-            <SideBarSection
-              active={activeSection === "technologies"}
-              title="Technologies"
-              onClick={() => handleSectionClick("technologies")}
-            />
-            <SideBarSection
-              active={activeSection === "contact"}
-              title="Contact"
-              onClick={() => handleSectionClick("contact")}
-            />
-          </>
+          ))}
         </VStack>
         {!closed && (
           <Box>
             <SideBarDescription
-              active={activeSection === "about"}
-              text="Risk GPT is a Large Language Model (LLM) powered chat bot. It is using knowledge contained in a local repository of documents to answer questions on topics of Finance and Risk. This is a proof of concept model that has not seen too many documents and therefore its capabilities are limited. To understand what topics Risk GPT Prototype has knowledge on, please refer to the Knowledge section."
-            />
-            <SideBarDescription
-              active={activeSection === "technologies"}
-              text="This prototype is powered by the following technologies: 1. LangChain 2. LLama Index 3. Vicuna Open Source LLM model 4. Flask Restful API 5. Typescript 6. Chakra UI"
-            />
-            <SideBarDescription
-              active={activeSection === "contact"}
-              text="For more information on this prototype please contact Sergei Margolin at example@email.com. You can find the source code under MIT license here: example@github.com."
+              active={activeSection === activeDescriptionKey}
+              text={activeDescriptionValue.text}
             />
           </Box>
         )}
